ec9050e7e226066ea3eccc4bc43cd20c8d3024f1
Vaughn Akiu <vkakiu@gmail.com>
2021-02-10 21:21:08 -0500

    modifying and adding to emerald ore loot tables through java
    -instead of using json used java for loot tables
    -adding two loot tables to emerald ore, adds on to existing instead of overwriting so emerald ore now drops 3 items

--- FabricTutorial/src/main/java/com/VaughnAkiu/tutorial/Tutorial.java
+++ FabricTutorial/src/main/java/com/VaughnAkiu/tutorial/Tutorial.java
@@ -2,12 +2,21 @@ package com.VaughnAkiu.tutorial;
 
 import com.VaughnAkiu.tutorial.registry.ModBlocks;
 import com.VaughnAkiu.tutorial.registry.ModItems;
+import jdk.vm.ci.meta.Constant;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gl.Uniform;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.util.Identifier;
 
 public class Tutorial implements ModInitializer
@@ -29,10 +38,37 @@ public class Tutorial implements ModInitializer
             })
             .build();
 
+    //grabbing emerald ore loot table id
+    private static final Identifier EMERALD_ORE_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/emerald_ore");
+    private static final Identifier RUBY_BLOCK_LOOT_TABLE_ID = new Identifier(MOD_ID, "blocks/ruby_block");
+
+
     @Override
     public void onInitialize()
     {
         ModItems.registerItems();
         ModBlocks.registerBlocks();
+        modifyLootTables();
+    }
+
+    private void modifyLootTables()
+    {
+        LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, identifier, fabricLootSupplierBuilder, lootTableSetter) ->
+        {   //checks for emerald ore loot table
+            if (EMERALD_ORE_LOOT_TABLE_ID.equals(identifier)){
+                //add single individual item
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(ItemEntry.builder(Items.GOLD_NUGGET))
+                        .withFunction(SetCountLootFunction.builder(UniformLootTableRange.between(1.0f, 4.0f)).build());
+                fabricLootSupplierBuilder.withPool(poolBuilder.build());
+
+                //add custom loot table
+                FabricLootPoolBuilder poolBuilderTwo = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(LootTableEntry.builder(RUBY_BLOCK_LOOT_TABLE_ID));
+                fabricLootSupplierBuilder.withPool(poolBuilderTwo.build());
+            }
+        }));
     }
 }
